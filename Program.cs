c5cbd5915ff27bed6d143f365c00ebe31b1bcae8
chuck francis <chuck_francis10@yahoo.com>
2020-08-04 11:14:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TheOrderOfThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number;
+            String opinion, size, age, shape, color, origin, material, purpose;
+            String noun;
+
+            number =  5.0;
+            opinion = " Awesome";
+            size = " teensy-weensy";
+            age = " new";
+            shape = " oblong";
+            color = " BRIGHT yellow";
+            origin = " AlphaCentaurain";
+            material = " platinum";
+            purpose = " good";
+
+            noun = " dragons";
+
+            // Using the + with strings, doesn't add it concatenates! (sticks thhem together)
+            Console.WriteLine(number + opinion + size + age + shape + color
+                + origin + material + purpose + noun);
+        }
+    }
+}
